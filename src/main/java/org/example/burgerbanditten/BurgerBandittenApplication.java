d99c3259bbd7d389a187c0e4b665473aa27db688
NikolajPedersen29 <mejlby2002@hotmail.com>
2026-05-04 09:14:12 +0200

    Started project

---
+++ src/main/java/org/example/burgerbanditten/BurgerBandittenApplication.java
@@ -0,0 +1,13 @@
+package org.example.burgerbanditten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerBandittenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BurgerBandittenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/burgerbanditten/BurgerBandittenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.burgerbanditten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgerBandittenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
