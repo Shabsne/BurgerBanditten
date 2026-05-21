dd35aa4e179e682641667a4a58d4f030b1a0c485
sebastianvij <sevi0001@stud.ek.dk>
2026-05-21 09:21:43 +0200

    Rettede rækkefølge i constructor i OrderServiceTests

--- src/test/java/org/example/burgerbanditten/order/OrderServiceTests.java
+++ src/test/java/org/example/burgerbanditten/order/OrderServiceTests.java
@@ -31,7 +31,7 @@ class OrderServiceTests {
         productRepository = mock(ProductRepository.class);
         emailService     = mock(EmailService.class);
         preOrderService = mock(PreOrderService.class);
-        orderService = new OrderService(orderRepository, emailService, productRepository, preOrderService);
+        orderService = new OrderService(orderRepository, emailService, preOrderService, productRepository);
     }
 
     // ── Ikke-eksisterende ordre ──────────────────────────
