b105d1fa742174770e4b9191fafd00f35b832741
Ullerboss <fullersted@gmail.com>
2026-05-25 14:17:45 +0200

    Trying to fix merge error

--- src/main/java/org/example/burgerbanditten/order/OrderService.java
+++ src/main/java/org/example/burgerbanditten/order/OrderService.java
@@ -306,5 +306,5 @@ public class OrderService {
         };
         // Genbrug den eksisterende findSalesOrders — ingen datofilter her
         return orderRepository.findSalesOrders(statuses, null, null);
-    }
+    } //huh
 }
